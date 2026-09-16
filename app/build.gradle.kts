948cf641054efab9deb28438a6ee408dfac7b45a
saanialam251-lab <saanialam251@gmail.com>
2026-09-17 00:28:53 +0530

    Upgrade compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ id("org.jetbrains.kotlin.plugin.compose")
 }
 android {
 namespace = "com.motiontracker"
-compileSdk = 34
+compileSdk = 35
 defaultConfig {
 applicationId = "com.motiontracker"
 minSdk = 24
-targetSdk = 34
+targetSdk = 35
 versionCode = 1
 versionName = "1.0"
 }
